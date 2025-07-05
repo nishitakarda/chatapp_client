b5d11ea49044e9131719c57cd65cd557125b8ba9
Nishita <nishitakarda4@gmail.com>
2025-07-05 17:04:06 +0530

    frontend backend integration

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import React , {useState} from 'react'
+import React , {useContext, useState} from 'react'
 import ChatContainer from '../components/ChatContainer'
 import Sidebar from '../components/Sidebar'
 import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
 
-    const [selectedUser, setSelectedUser] = useState(false);
+    const {selectedUser} = useContext(ChatContext)
 
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
@@ -13,9 +14,9 @@ const HomePage = () => {
              ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' :
                 'md:grid-cols-2'}`
         }>
-            <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
-            <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
-            <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+            <Sidebar />
+            <ChatContainer />
+            <RightSidebar />
         </div>
     </div>
   )
